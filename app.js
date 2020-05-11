1cb8763e5d0726f2f87752de44806a302f2ade0f
Rasheeq <contact@rasheeqahmed.com>
2020-05-11 11:19:20 -0700

    add routes

--- app.js
+++ app.js
@@ -1,12 +1,29 @@
 const express = require("express");
 const app = express();
-
-
-
-
-app.get("/", (req, res) => {
-    res.send("Hello World");
-})
+const mongoose = require("mongoose");
+const db = require("./config/keys").mongoURI;
+const users = require("./routes/api/users")
 
 const port = process.env.PORT || 5004;
 app.listen(port, () => console.log(`Server is running on port ${port}`));
+
+
+
+
+mongoose
+.connect(db, { useNewUrlParser: true, useUnifiedTopology: true })
+.then(() => console.log("Connected to MongoDB successfully"))
+.catch((err) => console.log(err));
+
+app.get("/", (req, res) => {
+    res.send("Hello World");
+})    
+
+app.use("/api/users", users);
+
+
+
+
+
+
+
