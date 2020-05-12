74b7987842593e1ea74fa315fa44153ffe62a30b
Rasheeq <contact@rasheeqahmed.com>
2020-05-12 10:43:57 -0700

    heroku setup

--- app.js
+++ app.js
@@ -6,6 +6,14 @@ const users = require("./routes/api/users");
 // const User = require('./models/User');
 const bodyParser = require('body-parser');
 const passport = require('passport');
+const path = require('path')
+
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static("frontend/build"));
+  app.get("/", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "frontend", "build", "index.html"));
+  });
+}
 
 
 mongoose
