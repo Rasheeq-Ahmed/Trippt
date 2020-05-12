2e84f30272b0dce931ec8f6f47ae73f7bb349cfa
your name here <echen831@gmail.com>
2020-05-12 15:41:39 -0700

    add trip_api_util.js and trip_actions.js

---
+++ frontend/src/actions/trip_actions.js
@@ -0,0 +1,45 @@
+import * as tripAPIUtils from '../util/trip_api_util';
+
+export const RECEIVE_TRIPS = 'RECEIVE_TRIPS';
+export const RECEIVE_TRIP = 'RECEIVE_TRIP';
+export const REMOVE_TRIP = 'REMOVE_TRIP';
+
+const receiveTrips = (trips) => ({
+    type: RECEIVE_TRIPS,
+    trips
+});
+
+const receiveTrip = (trip) => ({
+    type: RECEIVE_TRIP,
+    trip
+});
+
+const removeTrip = (id) => ({
+    type: REMOVE_TRIP,
+    id
+});
+
+export const getTrips = () => dispatch => (
+    tripAPIUtils.getTrips()
+        .then((trips) => dispatch(receiveTrips(trips)))
+);
+
+export const getTrip = (id) => dispatch => (
+    tripAPIUtils.getTrip(id)
+        .then((trip) => dispatch(receiveTrip(trip)))
+);
+
+export const createTrip = (data) => dispatch => (
+    tripAPIUtils.createTrip(data)
+        .then((trip) => dispatch(receiveTrip(trip)))
+);
+
+export const updateTrip = (data) => dispatch => (
+    tripAPIUtils.updateTrip(data)
+        .then((trip) => dispatch(receiveTrip(trip)))
+);
+
+export const removeTrip = (id) => dispatch => (
+    tripAPIUtils.removeTrip(id)
+        .then(() => dispatch(removeTrip(id)))
+)
