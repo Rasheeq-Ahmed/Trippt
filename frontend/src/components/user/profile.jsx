e20bd70feb75a16e2740527518b25058dd44fa4f
Rasheeq <contact@rasheeqahmed.com>
2020-05-13 10:30:46 -0700

    add favicon

---
+++ frontend/src/components/user/profile.jsx
@@ -0,0 +1,29 @@
+import React from 'react'
+
+
+class ProfilePage extends React.Component {
+    constructor(props) {
+        super(props)
+    }
+
+
+
+    render () {
+
+        return (
+
+            <div className="prof-all">hi
+
+
+
+            </div>
+            )
+
+    }
+
+
+
+
+}
+
+export default ProfilePage;
