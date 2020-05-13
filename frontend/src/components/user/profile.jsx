d6301dc0c5886634be9e90b8b37a3f707c1d1d22
Rasheeq <contact@rasheeqahmed.com>
2020-05-13 12:33:55 -0700

    add styling for Profile page

--- frontend/src/components/user/profile.jsx
+++ frontend/src/components/user/profile.jsx
@@ -1,20 +1,67 @@
 import React from 'react'
-
+import './profile.css'
 
 class ProfilePage extends React.Component {
     constructor(props) {
         super(props)
     }
 
+    tabClick() {
 
+    }
 
     render () {
-
+    
         return (
 
-            <div className="prof-all">hi
+            <div className="prof-all">
+                <div className="prof-body">
+                    <div className="prof-left">
+                        <div className="prof-pic">
+                            <img src="https://cdn.pixabay.com/photo/2015/10/05/22/37/blank-profile-picture-973460_1280.png" alt="" className="prof-img"/>
+                        </div>
+                        <div className="prof-details">
+                            <ul>
+                                <h2>Full Name</h2>
+                                <h4>@Username</h4>
+                                <p>San Franciso, CA</p>
+                                
+                            </ul>
+                        </div>
+                    </div>
 
 
+
+
+
+
+
+
+                    <div className="prof-right">
+                        <div className="prof-right-header">
+                            <div className="prof-tabs">
+                                <button className="tablinks" onClick=''>Trips</button>
+                                <button className="tablinks" onClick=''>Destinations</button>
+                                <button className="tablinks" onClick=''>Other</button>
+                            </div>
+                            
+                        </div>
+                        <div className="prof-right-body">
+                            <div id='trip' className="tabcontent">
+                                <h3>Trip 1</h3>
+                                <p> Details of Trip 1</p>
+                            </div>
+                            <div id="destination" className="tabcontent">
+                                <h3>Destination 1</h3>
+                                <p> Details of Destination 1</p>
+                            </div>
+                            <div id='other' className="tabcontent">
+                                <h3>Other 1</h3>
+                                <p> Details of Other 1</p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
 
             </div>
             )
