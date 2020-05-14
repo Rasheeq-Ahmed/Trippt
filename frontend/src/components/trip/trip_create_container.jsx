b9904825e013be30ffdea5f5339a79a0c126ed32
your name here <echen831@gmail.com>
2020-05-13 18:30:45 -0700

    get user trips is functional

--- frontend/src/components/trip/trip_create_container.jsx
+++ frontend/src/components/trip/trip_create_container.jsx
@@ -1,18 +1,20 @@
 import { connect } from 'react-redux';
-import { createTrip } from '../../actions/trip_actions';
+import { createTrip, getUserTrips } from '../../actions/trip_actions';
 import { getAttractions } from '../../actions/attraction_actions'
 import CreateTrip from './trip_create';
 
 const mapStateToProps = (state) => {
     return {
-       user: state.session.user
+       user: state.session.user,
+       trips: Object.values(state.trips)
     };
 };
 
 const mapDispatchToProps = dispatch => {
     return {
         createTrip: (data) => dispatch(createTrip(data)),
-        getAttractions: (locationId, city) => dispatch(getAttractions(locationId,city))
+        getAttractions: (locationId, city) => dispatch(getAttractions(locationId,city)),
+        getUserTrips: (user_id) => dispatch(getUserTrips(user_id))
     };
 };
 
--- frontend/src/reducers/root_reducer.js
+++ frontend/src/reducers/root_reducer.js
@@ -1,14 +1,14 @@
 import { combineReducers } from "redux";
 import session from "./session_reducer";
 import errors from "./errors_reducer";
-import trip from "./trip_reducer"
-import attraction from "./attraction_reducer"
+import trips from "./trip_reducer"
+import attractions from "./attraction_reducer"
 
 const RootReducer = combineReducers({
   errors,
   session,
-  trip,
-  attraction
+  trips,
+  attractions
 });
 
 export default RootReducer;
