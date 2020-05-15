4da3640ae3fb5803c0051f700e3ab2144eeee589
Rasheeq <contact@rasheeqahmed.com>
2020-05-15 08:22:22 -0700

    add styling to trips and profile page and attraction profile page

--- frontend/src/components/attraction/attraction_show.jsx
+++ frontend/src/components/attraction/attraction_show.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 import './attraction_show.css'
+import NavBar from '../nav/navbar';
 
 
 class AttractionShow extends React.Component{
@@ -8,71 +9,72 @@ class AttractionShow extends React.Component{
     super(props);
   }
 
-  // componentDidMount(){
-  //   this.props.getAttraction(this.props.locationId)
-  // };
+  componentDidMount(){
+    this.props.getAttraction(this.props.locationId)
+  };
 
   
 
 
   render(){
-    // if (!this.props.attraction) {
-    //   return null;
-    // }
+    if (!this.props.attraction) {
+      return null;
+    }
 
         
-    // const getPhotos = (photos) => {
-    //   for(var key in photos){
-    //     if(key === "images"){
-    //       return photos[key];
-    //     }
-    //   }
-    // }
+    const getPhotos = (photos) => {
+      for(var key in photos){
+        if(key === "images"){
+          return photos[key];
+        }
+      }
+    }
 
-    // const getImages = (obj) => {
-    //   for(var key in obj){
-    //     if(key === "medium"){
-    //       return obj[key]
-    //     }
-    //   }
-    // }
+    const getImages = (obj) => {
+      for(var key in obj){
+        if(key === "medium"){
+          return obj[key]
+        }
+      }
+    }
 
-    // const getUrl = (obj) => {
-    //   for(var key in obj){
-    //     if(key === 'url'){
-    //       return obj[key]
-    //     }
-    //   }
-    // }
+    const getUrl = (obj) => {
+      for(var key in obj){
+        if(key === 'url'){
+          return obj[key]
+        }
+      }
+    }
 
     return(
-        <div className="show">
+        <div className="show-all">
         <div className="show-header">
-          <h1>
-            <Link to="/">Trippt</Link>
-          </h1>
+         <NavBar/>
         </div>
         <div className="show-body">
-          <div className="show-inner-body">
-            
+          <div className="show-content">
+            <div className='show-left'>
+        <div>{this.props.attraction.name}
+          {/* {console.log(this.props)} */}
+          <div>{this.props.attraction.address}</div>
+          <div>{this.props.attraction.description}</div>
+          <div>{this.props.attraction.website}</div>
+          <div>{this.props.attraction.ranking}</div>
+          <div>{this.props.attraction.rating}</div>
+          <div>{this.props.attraction.recommended_visit_length}</div>
+        </div>
+              </div>
+            <div className="show-right">
+        <div className="show-photo">
+          <img src={`${getUrl(getImages(getPhotos(this.props.attraction.photo)))}`} alt=""/>
+        </div>
+            </div>
           </div>
         </div>
 
         <div className="show-footer"></div>
       </div>
         
-      //   <div className="show-photo">
-      //     <img src={`${getUrl(getImages(getPhotos(this.props.attraction.photo)))}`} alt=""/>
-      //   </div>
-      //   <div>{this.props.attraction.name}
-      //     {/* {console.log(this.props)} */}
-      //     <div>{this.props.attraction.address}</div>
-      //     <div>{this.props.attraction.description}</div>
-      //     <div>{this.props.attraction.website}</div>
-      //     <div>{this.props.attraction.ranking}</div>
-      //     <div>{this.props.attraction.rating}</div>
-      //     <div>{this.props.attraction.recommended_visit_length}</div>
-      //   </div>
       // </div>
     )
   }
--- frontend/src/components/nav/navbar.jsx
+++ frontend/src/components/nav/navbar.jsx
@@ -19,7 +19,7 @@ class NavBar extends React.Component {
       if (this.props.loggedIn) {
         return (
             <div className="nav-auth">
-                <Link to='/create'><button>Trips</button></Link>
+                <Link to='/profile'><button>Profile</button></Link>
                 <Link><button onClick={this.logoutUser}>Logout</button></Link>
             </div>
         );
