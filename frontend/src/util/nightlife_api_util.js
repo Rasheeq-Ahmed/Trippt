e28029798beadf224451d6c2fe902baac1afb8b4
Rasheeq <contact@rasheeqahmed.com>
2020-06-01 14:13:03 -0700

    reduce API calls for testing purposes

--- frontend/src/util/nightlife_api_util.js
+++ frontend/src/util/nightlife_api_util.js
@@ -14,7 +14,7 @@ export const getNightlife = (locationId) => {
     "currency":"USD",
     "sort":"ranking",
     "lunit":"km",
-    "limit": 20,
+    "limit": 3,
     "subcategory":"20",
     "location_id": `${locationId}`
     }
