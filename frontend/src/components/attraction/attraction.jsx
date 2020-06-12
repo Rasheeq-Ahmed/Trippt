d3d5ade773c69f9884ff2909c1a0485d9629b720
Hari Sachdeva <hsachdevaucd@gmail.com>
2020-06-12 11:26:13 -0700

    Update attraction page

--- frontend/src/components/attraction/attraction.jsx
+++ frontend/src/components/attraction/attraction.jsx
@@ -79,7 +79,7 @@ class Attraction extends React.Component{
     // {console.log(this.findTripId(this.props.userTrips, this.props.locationId))}
     // {console.log(this.props)}
     let myTrips = this.findTripId(this.props.userTrips, this.props.locationId) // array of tripIds
-    console.log(myTrips);
+    // console.log(myTrips);
     let attractions = this.props.attractions[this.props.locationName];
     // let restaurants = this.props.restaurants[this.props.locationName];
     let nightlife = this.props.nightlife[this.props.locationName];
