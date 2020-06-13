53bbcf589d783a3294da23edf417b2123fc5e555
Rasheeq <contact@rasheeqahmed.com>
2020-06-12 18:47:01 -0700

    testing

--- frontend/src/components/nav/test.jsx
+++ frontend/src/components/nav/test.jsx
@@ -1,26 +1,20 @@
 import React, { Component } from 'react'
 import './toggle.css'
-import Toggle from './toggleRP'
+import Toggle from './toggle-RPC'
 export class Test extends Component {
 
     render() {
         return (
             <div className='all'>
                <div className='testing'>
-                    <Toggle render={({on, toggle}) => (
-                        <div>
-                        { on && <h1>Show me</h1>}
-                        <button onClick={toggle}>Show/Hide</button>
-                        </div>
-                    )}/>
-                    <Toggle render={({ on, toggle }) => (
-                        <div>
-                            {on && <nav>nav me</nav>}
-                            <button onClick={toggle}>nav</button>
-                        </div>
-                    )} />
-
-
+                    <Toggle>
+                       { ({on, toggle}) => (
+                            <div>
+                            { on && <h1>Show me</h1>}
+                            <button onClick={toggle}>Show/Hide</button>
+                            </div>
+                        )}
+                    </Toggle> 
                </div>
             </div>
         )
