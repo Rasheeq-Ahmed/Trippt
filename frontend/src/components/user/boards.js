b7e489ad5b6b69b774b24513e066fa573312475f
Rasheeq <contact@rasheeqahmed.com>
2020-06-14 14:25:11 -0700

    add simple drag and drop

---
+++ frontend/src/components/user/boards.js
@@ -0,0 +1,31 @@
+import React from 'react'
+
+function Board(props) {
+    const drop = e => {
+        e.preventDefault();
+        const card_id = e.dataTransfer.getData('card_id');
+        
+        const card = document.getElementById(card_id)
+        card.style.display = 'block';
+
+        e.target.appendChild(card);
+
+    }
+
+    const dragOver = e => {
+        e.preventDefault();
+
+    }
+
+    return (
+        <div id ={props.id}
+            onDrop={drop}
+            onDragOver={dragOver}
+            className={props.className}
+        >
+            {props.children}
+        </div>
+    )
+}
+
+export default Board;
