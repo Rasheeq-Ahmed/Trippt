83dd42ee4765f743242fc947657c09172bdbf603
your name here <echen831@gmail.com>
2020-06-17 21:30:40 -0700

    add profile backend

---
+++ frontend/src/reducers/profile_reducer.js
@@ -0,0 +1,16 @@
+import { RECEIVE_USER_PROFILE } from '../actions/profile_actions'
+
+const ProfileReducer = (state = {}, action) => {
+    Object.freeze(state)
+
+    switch (action.type) {
+        case RECEIVE_USER_PROFILE:
+            
+            return Object.assign({}, state, action.data.data);
+    
+        default:
+            return state;
+    }
+}
+
+export default ProfileReducer
